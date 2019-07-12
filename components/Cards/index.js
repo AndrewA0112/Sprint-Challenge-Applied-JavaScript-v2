9cdb3efffe96ecd91b888a4f75e79fcf793f76c2
Andrew <andrew.mkal@outlook.com>
2019-07-12 10:53:23 -0500

    Completed Cards/index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,14 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cards = document.querySelector('.cards-container')
 
 const articlesPromise = axios.get('https://lambda-times-backend.herokuapp.com/articles')
 
 articlesPromise
     .then(data => {
-        console.log('API response', data)
+        console.log('Articles API response', data.data.articles)
+        for (let [type, articles] of Object.entries(data.data.articles)) {
+            console.log(type, articles);
+            articles.forEach(article => {
+                cards.appendChild(createCard(article))
+            })
+        }
     })
 
     .catch(error => {
         console.log('Error connecting to API', error)
     })
+
+function createCard(obj) {
+    const card = document.createElement('div')
+    const cardHeadline = document.createElement('div')
+    const cardAuthorCont = document.createElement('div')
+    const cardImgCont = document.createElement('div')
+    const cardImg = document.createElement('img')
+    const cardAuthor = document.createElement('span')
+
+    card.classList.add('card')
+    cardHeadline.classList.add('headline')
+    cardAuthorCont.classList.add('author')
+    cardImgCont.classList.add('img-container')
+
+    cardHeadline.textContent = obj.headline
+    cardImg.src = obj.authorPhoto
+    cardAuthor.textContent = obj.authorName
+
+    card.appendChild(cardHeadline)
+    card.appendChild(cardAuthorCont)
+
+    cardAuthorCont.appendChild(cardImgCont)
+    cardAuthorCont.appendChild(cardAuthor)
+
+    cardImgCont.appendChild(cardImg)
+
+    return card
+}
